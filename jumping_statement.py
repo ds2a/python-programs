d94349241be8586c8d14cf4b905c813b441144fa
sreenu <peramsrinivasareddy789@gmail.com>
2024-08-13 13:11:45 +0530

    jumping statement

---
+++ jumping_statement.py
@@ -0,0 +1,23 @@
+"jumping statements in pythin life youtube channel"
+
+" pass statement "
+
+# if True:
+#     pass
+
+"break statement"
+# for i in "pythonlife":
+#    if i == 'l':
+#        break
+#    print(i)
+
+"continue statement"
+for i in "pythonlife":
+    if i == 'l':
+        continue
+    print(i)
+    '''
+    pass -- ignore the statement 
+    break -- terminate the statement after iteration
+    continue -- it will skip the iteration and execute the remaning   
+    '''
